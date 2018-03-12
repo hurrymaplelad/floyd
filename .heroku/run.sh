f84275ab4847d56c0941414437615a7ca51bce04
Adam Hull <adam@hmlad.com>
2018-03-11 18:51:53 -0700

    Configure rclone

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -7,4 +7,11 @@
 # https://rclone.org/install/#script-installation
 curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
-cp rclone-*-linux-amd64/rclone .
+mkdir -p shellbin
+cp rclone-*-linux-amd64/rclone shellbin
+
+# Put rclone on the path
+# https://blog.heroku.com/hacking-buildpacks#modifying-the-path
+echo "---"
+echo "config_vars:"
+echo "  PATH: $PATH:$PWD/shellbin"
